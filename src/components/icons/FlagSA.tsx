d078ceed439fb2c25d598db69bda4da14d94bfc9
unknown <RayaneTks>
2025-11-09 23:37:28 +0100

    fix flag

--- src/components/icons/FlagSA.tsx
+++ src/components/icons/FlagSA.tsx
@@ -5,37 +5,32 @@ export const FlagSA = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     aria-label="Saudi Arabia flag"
   >
-    <path fill="#006c35" d="M0 0h640v480H0z" />
-    <path fill="#fff" d="M0 0h640v320H0z" />
-    <g id="c" transform="translate(213.3 240)">
-      <path
-        id="a"
-        fill="none"
-        stroke="#006c35"
-        strokeWidth="3"
-        d="m-10.5 2.2 4.2 13.6-13.5-4.1 13.5-4.1-4.2 13.6"
-      />
-      <use href="#a" transform="rotate(45)" />
-      <use href="#a" transform="rotate(90)" />
-      <use href="#a" transform="rotate(135)" />
-      <path id="b" fill="#006c35" d="M0 14.2v21.3" />
-      <use href="#b" transform="rotate(22.5)" />
-      <use href="#b" transform="rotate(45)" />
-      <use href="#b" transform="rotate(67.5)" />
+    {/* Fond vert saoudien */}
+    <rect width="640" height="480" fill="#006C35"/>
+    
+    {/* Épée blanche horizontale pointant vers la gauche (côté mât) */}
+    <g transform="translate(320, 240)">
+      {/* Lame de l'épée */}
+      <rect x="-160" y="-10" width="320" height="20" fill="#ffffff" rx="2"/>
+      
+      {/* Garde de l'épée (verticale au centre) */}
+      <rect x="-15" y="-28" width="30" height="56" fill="#ffffff" rx="3"/>
+      
+      {/* Poignée (à droite) */}
+      <rect x="160" y="-12" width="50" height="24" fill="#ffffff" rx="3"/>
+      
+      {/* Pointe de l'épée (triangle pointant vers la gauche) */}
+      <path d="M -160 -10 L -190 0 L -160 10 Z" fill="#ffffff"/>
     </g>
-    <use href="#c" transform="scale(-1 1)" />
-    <path
-      fill="#006c35"
-      d="M213.3 271.3c-4.2 0-7.6-3.4-7.6-7.6s3.4-7.6 7.6-7.6 7.6 3.4 7.6 7.6-3.4 7.6-7.6 7.6z"
-      transform="scale(-1 1)"
-    />
-    <path
-      fill="none"
-      stroke="#006c35"
-      strokeWidth="2"
-      d="M213.3 240v30M213.3 240v30"
-      transform="scale(-1 1)"
-    />
+    
+    {/* Shahada (profession de foi) - représentation stylisée du texte arabe calligraphique */}
+    <g transform="translate(320, 140)" fill="#ffffff" opacity="0.95">
+      {/* Motifs calligraphiques représentant la shahada */}
+      <rect x="-180" y="-6" width="360" height="12" rx="6"/>
+      <rect x="-160" y="-14" width="320" height="8" rx="4" opacity="0.7"/>
+      <rect x="-140" y="8" width="280" height="8" rx="4" opacity="0.7"/>
+      <rect x="-120" y="-20" width="240" height="6" rx="3" opacity="0.5"/>
+      <rect x="-100" y="14" width="200" height="6" rx="3" opacity="0.5"/>
+    </g>
   </svg>
 );
-
